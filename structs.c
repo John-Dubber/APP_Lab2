9ffca6957a37339236ac07e57a7f6db020b76b8f
John <jonnydubber@gmail.com>
2020-01-30 17:30:03 +0000

    appending files

--- structs.c
+++ structs.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<curses.h>
 
-void main()
+int main()
 {
     typedef struct
     {
@@ -20,7 +21,7 @@ void main()
     company_t company[numCompanies];
 
 	//1. Open the file.
-	outFile = fopen("CompanyDetails.txt", "w");
+	outFile = fopen("CompanyDetails.txt", "a");
 
     /* FILE HEADER IF NEEDED
     fprintf(outFile,"%-20s%-8s%-8s","Company Name","Reg","VAT");
@@ -42,7 +43,7 @@ void main()
 		for (int i = 0; i < numCompanies; i++)
         {
             printf("Please Enter Company Name, Registration Number, Vat Number of company\n");
-            scanf(" %s%d%d", &company[i].name, &company[i].regNum, &company[i].vatNum);
+            scanf(" %s%d%d", company[i].name, &company[i].regNum, &company[i].vatNum);
             fprintf(outFile," %-20s%-8d%-8d", company[i].name, company[i].regNum, company[i].vatNum);
             printf("Please enter sales history for company for the past 12 months format 1.11 2.22 3.33 4.44...\n");
             for(int j=0;j<12;j++)
@@ -50,9 +51,11 @@ void main()
                 scanf(" %f", &company[i].sales[j]);
                 fprintf(outFile," %-8.2f", company[i].sales[j]);
             } 
+            fprintf(outFile,"\n");
         }
 		//Close the file only when finished reading...
 		fclose(outFile);
 	}
+    return 0;
 }
 
