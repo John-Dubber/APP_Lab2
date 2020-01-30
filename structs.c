e44371464b33bd73447d72ec8aa0b8c3eaa229bc
John <jonnydubber@gmail.com>
2020-01-30 17:34:51 +0000

    fixed formatting on structs.c

--- structs.c
+++ structs.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<curses.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <curses.h>
 
 int main()
 {
@@ -10,18 +10,17 @@ int main()
         int regNum;
         int vatNum;
         float sales[12];
-    }company_t;
-    
-    
-	FILE* outFile;
+    } company_t;
+
+    FILE *outFile;
     int numCompanies;
 
     printf("How many companies do you wish to input?");
     scanf("%d", &numCompanies);
     company_t company[numCompanies];
 
-	//1. Open the file.
-	outFile = fopen("CompanyDetails.txt", "a");
+    //1. Open the file.
+    outFile = fopen("CompanyDetails.txt", "a");
 
     /* FILE HEADER IF NEEDED
     fprintf(outFile,"%-20s%-8s%-8s","Company Name","Reg","VAT");
@@ -31,31 +30,30 @@ int main()
     }
     fprintf(outFile,"\n");*/
 
-	//2. Check the file is opened.
-	if (outFile == NULL)
-	{
-		printf("Sorry the file could not be opened\n");
-	}
+    //2. Check the file is opened.
+    if (outFile == NULL)
+    {
+        printf("Sorry the file could not be opened\n");
+    }
 
-	else
-	{
-		//while (feof(inputfile) == false)
-		for (int i = 0; i < numCompanies; i++)
+    else
+    {
+        //while (feof(inputfile) == false)
+        for (int i = 0; i < numCompanies; i++)
         {
             printf("Please Enter Company Name, Registration Number, Vat Number of company\n");
             scanf(" %s%d%d", company[i].name, &company[i].regNum, &company[i].vatNum);
-            fprintf(outFile," %-20s%-8d%-8d", company[i].name, company[i].regNum, company[i].vatNum);
+            fprintf(outFile, " %-20s%-8d%-8d", company[i].name, company[i].regNum, company[i].vatNum);
             printf("Please enter sales history for company for the past 12 months format 1.11 2.22 3.33 4.44...\n");
-            for(int j=0;j<12;j++)
+            for (int j = 0; j < 12; j++)
             {
                 scanf(" %f", &company[i].sales[j]);
-                fprintf(outFile," %-8.2f", company[i].sales[j]);
-            } 
-            fprintf(outFile,"\n");
+                fprintf(outFile, " %-8.2f", company[i].sales[j]);
+            }
+            fprintf(outFile, "\n");
         }
-		//Close the file only when finished reading...
-		fclose(outFile);
-	}
+        //Close the file only when finished reading...
+        fclose(outFile);
+    }
     return 0;
 }
-
